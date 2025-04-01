dd33dd4e0d8cb4da686d1a7c0277fc5214328ec2
Amdragz <ogaji2006@gmail.com>
2025-04-01 19:54:26 +0100

    chore(assets/images/logo): added the fiya logo to assets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div>
-    I am Home
-  </div>
+  <RouterView />
 </template>
 
 <style scoped></style>
