6a47045dffb6cdac8a2b663305c07c30ddbba9c0
Amdragz <ogaji2006@gmail.com>
2025-04-02 22:46:47 +0100

    feat(views): implemented the auth pages

---
+++ src/views/auth/AuthPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="auth-page">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.auth-page {
+  background-image: url('@/assets/images/illustrations/fiya-auth-background.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
--- src/views/dashboard/HomeView.vue
+++ src/views/dashboard/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import FBtn from '@/components/system/form/FBtn.vue';
+
 
 </script>
 
 <template>
   <div>
+    <FBtn>
+      boys
+    </FBtn>
   </div>
 </template>
 
